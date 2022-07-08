3f951fefed5630d922ebb5181741997a2fe7c0a7
Mauricio <msevillab@gmail.com>
2022-07-08 14:04:36 -0500

    primera versión funcional sistema de examen en línea

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "¿Cuál de las siguientes bases de datos ha sido compatible con PHP casi desde el principio?",
+    answer: "MySQL",
+    options: [
+      "Oracle Database",
+      "SQL",
+      "MySQL",
+      "SQL+"
+    ]
+  },
+    {
+    numb: 2,
+    question: "La extensión de MySQL actualizada lanzada con PHP 5 generalmente se conoce como...",
+    answer: "mysqli",
+    options: [
+      "mysqli",
+      "MySQL",
+      "mysql",
+      "mysqly"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cuál de las siguientes líneas debe descomentarse o agregarse en el archivo php.ini para habilitar la extensión mysqli?",
+    answer: "extension=php_mysqli.dll",
+    options: [
+      "extension=mysql.dll",
+      "extension=php_mysqli.dll",
+      "extension=php_mysqli.dl",
+      "extension=mysqli.dl"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿En qué versión de PHP se introdujo MySQL Native Driver (también conocido como mysqlnd)?",
+    answer: "PHP 5.3",
+    options: [
+      "PHP 5.3",
+      "PHP 5.0",
+      "PHP 5.1",
+      "PHP 5.2"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Cuál de las siguientes afirmaciones se utiliza para crear una tabla?",
+    answer: "CREATE TABLE table_name (column_name column_type);",
+    options: [
+      "CREATE table_name (column_type column_name);",
+      "CREATE table_name (column_name column_type);",
+      "CREATE TABLE table_name (column_type column_name);",
+      "CREATE TABLE table_name (column_name column_type);"
+    ]
+  },
+  // puede descomentar los códigos a continuación y duplicarlos tanto como desee agregar una pregunta
+  // pero recuerde que debe dar el valor adormecido serializar como 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Tu pregunta está aquí",
+  //   answer: "La respuesta correcta de la pregunta está aquí.",
+  //   options: [
+  //     "Opción 1",
+  //     "Opción 2",
+  //     "Opción 3",
+  //     "Opción 4"
+  //   ]
+  // },
+];
